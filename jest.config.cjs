d0eddb57d1e74a4b16e1a5c5d1cda9ea5763444b
Oleksandr Rabovskyi <>
2021-10-13 19:08:10 +0300

    Update jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,10 @@
+let timeout = process.env.TIMEOUT || 60100
+
 const config = {
     verbose: true,
+    testTimeout: timeout,
+    setupFiles: ["dotenv/config"],
+    reporters: ["default", "jest-allure"]
 };
   
 module.exports = config;
