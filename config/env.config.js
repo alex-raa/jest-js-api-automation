cc72ddb4bee29a3a2cf69b5eecc37bca26e580f1
Oleksandr Rabovskyi <>
2021-10-22 09:55:48 +0300

    added secret key test

--- config/env.config.js
+++ config/env.config.js
@@ -8,5 +8,9 @@ export const CONFIG = cleanEnv(process.env, {
     API_URL: url({
       default: process.env.DOMAIN_API_URL || `https://cci-gwp-adonis-api.herokuapp.com`,
       desc: "API URL to be used"
+    }),
+    S_KEY: str({
+      default: process.env.SECRET_KEY,
+      desc: "secret key from github"
     })
 });
--- tests/dev-tests/users.test.js
+++ tests/dev-tests/users.test.js
@@ -1,5 +1,7 @@
 import { UsersController } from "../../api/controllers/usersController.js";
+import { CONFIG } from "../../config/env.config";
 
+const secretKey = CONFIG.S_KEY;
 let firstUser;
 
 describe("Check Users api functionality", function() {
@@ -9,21 +11,25 @@ describe("Check Users api functionality", function() {
         expect(message).toEqual("Welcome to the Adonis API tutorial")
 	});
     
-    it("Chek Users list", async function() {
+    it("Check Users list", async function() {
 		const response = await UsersController.getUsers();
         firstUser = response[0].id;
         let allUsersAmount = response.length;
         expect(allUsersAmount).toBeGreaterThan(600)
 	});
 
-    it("Chek user by Id", async function() {
+    it("Check user by Id", async function() {
 		const response = await UsersController.getUserById(firstUser);
         expect(response[0].username).toEqual("stan")
         expect(response[0].email).toEqual("ndagis@gmail.com")
 	});
 
-    it("Chek user not existed", async function() {
+    it("Check user not existed", async function() {
 		const response = await UsersController.getUserById("99192192912912");
         expect(response.length).toEqual(0)
+	});
+
+    it("Check Github Actions secrets", async function() {
+		console.log("here is secret KEY: ", secretKey);
 	});
 });
